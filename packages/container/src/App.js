b932a09329f15ae7262718c299c3417ef2727502
Alejandro Rodriguez <alejandro.rodriguez.escu@gmail.com>
2022-01-20 20:10:32 +0100

    feat: Finish feature #1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
-// testing CI/CD
 export default () => {
   return (
     <>
-      <h1>Hey there!</h1>
+      <h1>Hey there!!!!!!!!!!</h1>
       <hr />
       <MarketingApp />
     </>
